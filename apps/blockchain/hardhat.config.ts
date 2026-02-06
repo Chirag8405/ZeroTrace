fb6c63a66fbe70f68f7072c6d152658c78f10938
Jay Kerkar <bytewise0405@gmail.com>
2026-02-06 11:50:43 +0530

    Refactor code structure for improved readability and maintainability

--- apps/blockchain/hardhat.config.ts
+++ apps/blockchain/hardhat.config.ts
@@ -1,7 +1,18 @@
+import * as dotenv from "dotenv";
 import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+dotenv.config();
 
 export default defineConfig({
-  solidity: {
-    version: "0.8.28",
+  plugins: [hardhatToolboxMochaEthers],
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      type: "http",
+      chainId: 11155111,
+      url: process.env.SEPOLIA_RPC_URL!,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 });
